9cbed8f69c0fa06db8b4c4bf525b3783926ae2d2
kannandin <37356111+kannandin@users.noreply.github.com>
2018-09-25 22:27:13 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+void main()
+{
+     char a;
+    scanf("%c",&a);
+    if(a=='a' ||a=='e' || a=='i' ||a=='o' || a=='u')
+    {
+        printf("Vowel");
+        
+    }
+    else
+    {
+        printf("Consonant");
+    }
+}
